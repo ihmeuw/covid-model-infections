00ff8cf205420ea2ba4a545b6a8c7fa2d5d5e2f7
Ryan Barber <rmbarber@uw.edu>
2021-03-16 12:38:13 -0700

    fix merger root

--- src/covid_model_infections/pdf_merger.py
+++ src/covid_model_infections/pdf_merger.py
@@ -4,16 +4,16 @@ import os
 from PyPDF2 import PdfFileMerger
 
 
-def pdf_merger(pdfs: List, location_names: List, parent_names: List, outfile: str):
+def pdf_merger(pdfs: List, location_names: List, parent_names: List, levels: List, outfile: str):
     # how are inputs specified
     assert all([i.endswith('.pdf') for i in pdfs]), 'Not all files passed into `pdfs` are actual PDFs.'
     indir = '/'.join(pdfs[0].split('/')[:-1])
 
     # compile PDFs
     merger = PdfFileMerger()
-    for i, (pdf, location_name, parent_name) in enumerate(zip(pdfs, location_names, parent_names)):
+    for i, (pdf, location_name, parent_name, level) in enumerate(zip(pdfs, location_names, parent_names, levels)):
         merger.append(pdf)
-        if parent_name in location_names and i > 0:
+        if parent_name in location_names and level > 0:
             if parent_name == location_name:
                 merger.addBookmark(f'{location_name} ', i, parent_name)
             else:
@@ -22,10 +22,7 @@ def pdf_merger(pdfs: List, location_names: List, parent_names: List, outfile: st
             merger.addBookmark(location_name, i)
 
     # get output file (if already exists, delete before writing new file)
-    if outfile is None:
-        outfile = f'{indir}/_compiled.pdf'
-    else:
-        assert outfile.endswith('.pdf'), 'Provided output file is not a PDF.'
+    assert outfile.endswith('.pdf'), 'Provided output file is not a PDF.'
     if os.path.exists(outfile):
         os.remove(outfile)
 
