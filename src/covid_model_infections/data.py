baf78030e04f31e070c124ea8a73e489e9a0f1fb
Ryan Barber <rmbarber@uw.edu>
2021-04-07 11:51:52 -0700

    arkansas admmissions, not ohio

--- src/covid_model_infections/data.py
+++ src/covid_model_infections/data.py
@@ -96,10 +96,10 @@ def evil_doings(data: pd.DataFrame, hierarchy: pd.DataFrame, input_measure: str)
         # data = data.loc[~is_bc].reset_index(drop=True)
         # manipulation_metadata['british_columbia'] = 'dropped all hospitalizations'
         
-        is_ohio = data['location_id'] == 558
+        is_arkansas = data['location_id'] == 526
         is_pre_mar1 = data['date'] < pd.Timestamp('2020-03-01')
-        data = data.loc[~(is_ohio & is_pre_mar1)].reset_index(drop=True)
-        manipulation_metadata['ohio'] = 'dropped leading 0s in admissions from Ohio (pre March 1)'
+        data = data.loc[~(is_arkansas & is_pre_mar1)].reset_index(drop=True)
+        manipulation_metadata['arkansas'] = 'dropped leading 0s in admissions from Arkansas (pre March 1)'
     
     elif input_measure == 'deaths':
         uk_location_ids = hierarchy.loc[hierarchy['path_to_top_parent'].apply(lambda x: '95' in x.split(',')),
@@ -109,10 +109,10 @@ def evil_doings(data: pd.DataFrame, hierarchy: pd.DataFrame, input_measure: str)
         data = data.loc[~(is_uk & is_pre_mar1)].reset_index(drop=True)
         manipulation_metadata['uk'] = 'dropped leading 0s in deaths from UK (pre March 1)'
         
-        is_arkansas = data['location_id'] == 526
-        is_pre_mar15 = data['date'] < pd.Timestamp('2020-03-15')
-        data = data.loc[~(is_arkansas & is_pre_mar15)].reset_index(drop=True)
-        manipulation_metadata['arkansas'] = 'dropped leading 0s in deaths from Arkansas (pre March 15)'
+        is_ohio = data['location_id'] == 558
+        is_pre_mar1 = data['date'] < pd.Timestamp('2020-03-01')
+        data = data.loc[~(is_ohio & is_pre_mar1)].reset_index(drop=True)
+        manipulation_metadata['ohio'] = 'dropped leading 0s in deaths from Ohio (pre March 1)'
     
     else:
         raise ValueError(f'Input measure {input_measure} does not have a protocol for exclusions.')
