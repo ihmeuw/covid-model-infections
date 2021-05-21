53a5085feeb103f8c05b05833e84f62a320d1806
Steve Bachmeier <sbachmei@uw.edu>
2021-05-21 10:18:44 -0700

    Remove testing comments

--- src/covid_model_infections/cluster.py
+++ src/covid_model_infections/cluster.py
@@ -11,9 +11,7 @@ from loguru import logger
 
 
 PROJECT = 'proj_covid_prod'
-# PROJECT = 'proj_covid' # steve testing
 QUEUE = 'all.q'
-# QUEUE = 'd.q' # steve testing
 F_MEM = '5.0G'
 F_THREAD = '17'
 OMP_NUM_THREADS = '17'
